9ff77031d0107bb061e3854ec0ff26c06fb6f47a
bkkavin <bkkavin2000@gmail.com>
2021-12-16 14:34:59 +0530

    dotfiles update

--- dot.sh
+++ dot.sh
@@ -4,8 +4,6 @@ cp ~/dot.sh ~/git/dotfiles/
 cp ~/Scripts/* ~/git/dotfiles/
 cd ~/git/dotfiles/
 eval ' ssh-agent -s'
-#exec ssh-agent
-#~/gssh.sh
 git commit -a -m "dotfiles update"
 git push
 cd ~ 
