bbadec42ce384d9e85b71ced944c003dd80ffb13
bkkavin <bkkavin2000@gmail.com>
2021-12-16 15:12:48 +0530

    dotfiles update

--- init.el
+++ init.el
@@ -80,7 +80,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(which-key rainbow-delimiters use-package no-littering ivy doom-modeline command-log-mode auto-package-update)))
+   '(counsel ivy-rich which-key rainbow-delimiters use-package no-littering ivy doom-modeline command-log-mode auto-package-update))
+ '(which-key-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -93,5 +94,13 @@
        :config
   (setq which-key-idle-delay 0))
 
+(use-package ivy-rich
+  :init
+  (ivy-rich-mode 1))
 
-:bkk
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)
+	 ("C-x b" . counsel-ibuffer)
+	 ("C-x C-f" . counsel-find-file)
+	 :map minibuffer-local-map
+	 ("C-r" . 'counsel-minibuffer-history)))
