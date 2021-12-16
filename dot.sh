1ba7b83ad26771acfe74a85eeb7673a411e8b355
bkkavin <bkkavin2000@gmail.com>
2021-12-16 14:18:05 +0530

    dotfiles update

---
+++ dot.sh
@@ -0,0 +1,10 @@
+cp ~/.bashrc ~/git/dotfiles/
+cp ~/.emacs.d/init.el ~/git/dotfiles/
+cp ~/dot.sh ~/git/dotfiles/
+cd ~/git/dotfiles/
+#eval $(ssh-agent)
+#exec ssh-agent
+~/gssh.sh
+git commit -a -m "dotfiles update"
+git push
+cd 
