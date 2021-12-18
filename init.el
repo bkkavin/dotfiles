4e9743dfa54c5829577368794b99f768ef45a494
bkkavin <bkkavin2000@gmail.com>
2021-12-18 17:29:23 +0530

    dotfiles update

--- init.el
+++ init.el
@@ -193,11 +193,4 @@
   :custom
   (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
 
-(use-package evil-magit
-  :after magit)
-
-;; NOTE: Make sure to configure a GitHub token before using this package!
-;; - https://magit.vc/manual/forge/Token-Creation.html#Token-Creation
-;; - https://magit.vc/manual/ghub/Getting-Started.html#Getting-Started
 (use-package forge)
-bkk
