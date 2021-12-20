e076fc064b94894acf9366e229dc3f3fa2bd9033
bkkavin <bkkavin2000@gmail.com>
2021-12-20 17:56:03 +0530

    dotfiles update

--- init.el
+++ init.el
@@ -148,13 +148,14 @@
  ;(setq evil-want-C-i-jump nil)
  
 :config
- (evil-mode 1)) 
+ (evil-mode 1) 
  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state) 
  (define-key evil-insert-state-map (kbd "C-m") 'evil-delete-backward-char-and-join)
+ (define-key evil-insert-state-map (kbd "RET") 'newline)
  ;; Use visual line motions even outside of visual-line-mode buffers
   (evil-global-set-key 'motion "j" 'evil-next-visual-line)
   (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
-  
+ 
 (use-package evil-collection 
 :after evil
 :config
