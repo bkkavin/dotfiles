0c9c7f71b54fe86a3c0b1191e500a809b16059a2
bkkavin <bkkavin2000@gmail.com>
2021-12-20 22:13:10 +0530

    dotfiles update

--- dot.sh
+++ dot.sh
@@ -2,7 +2,7 @@ cp ~/git/dotfiles/.bashrc ~
 cp ~/git/dotfiles/init.el ~/.emacs.d/init.el 
 cp ~/git/dotfiles/dot.sh ~
 cp ~/git/dotfiles/.Xmodmap ~
-cp ~/Scripts/* ~/git/dotfiles/
+#cp ~/Scripts/* ~/git/dotfiles/
 cd ~/git/dotfiles/
 eval ' ssh-agent -s'
 git commit -a -m "dotfiles update"
