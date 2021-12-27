024ed8d344e1fcde557fe4d27a2a2f77b073a6ac
bkkavin <bkkavin2000@gmail.com>
2021-12-27 22:44:24 +0530

    dotfiles update

--- xmonad.hs
+++ xmonad.hs
@@ -51,21 +51,23 @@ myLayoutHook = noBorders(Full) |||  ThreeCol 1 (3/100) (1/3) {-||| ThreeColMid 1
 myTerminal = "urxvt"
 myModMask  = mod4Mask
 myManageHook = manageSpawn <+> manageHook def <+> composeAll 
+             [ appName =? "emacs" --> doRectFloat (W.RationalRect 0.25 0.25 0.5 0.5)] 
 
-myStartupHook = do
+
+(-myStartupHook = do
 
 {-spawnAndDo manageSpawn "emacs" -}
 
-{-spawnOn "1" "emacs"-}
+(-spawnOn "1" "emacs"-}
 spawnOn "2" "brave"
 spawnOn "3" "urxvt"
-spawnOn "4" "urxvt ~/echo.sh"
+spawnOn "4" "urxvt ~/echo.sh" -}
 {-manageHook :: manageHook
 manageHook = composeAll
 -}
 
-myManageHook = composeAll $ 
-    [ appName =? "emacs" --> doRectFloat (W.RationalRect 0.25 0.25 0.5 0.5)] 
+
+
    
 
 {- myManageHook :: ManageHook                                      
