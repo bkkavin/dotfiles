d8c228804fa984336ab7719a0e271512486d1ca7
bkkavin <bkkavin2000@gmail.com>
2021-12-28 17:23:32 +0530

    dotfiles update

--- xmonad.hs
+++ xmonad.hs
@@ -49,7 +49,7 @@ defaults = def
 myLayoutHook = noBorders(Full) |||  ThreeCol 1 (3/100) (1/3) {-||| ThreeColMid 1 (3/100) (1/2) -} ||| magnifier (Tall 1 (3/100) (1/2)) 
 myTerminal = "urxvt"
 myModMask  = mod4Mask
- myManageHook = manageHook def 
+ myManageHook = manageHook def <+> manageSpawn 
 {- myManageHook =+ {- manageSpawn <+> -}  manageHook def {- <+> composeOne -}
               [appName =? "emacs" --> doRectFloat (W.RationalRect 0.25 0.25 0.5 0.5)]  -}
 
