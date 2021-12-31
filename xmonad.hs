7a07ffbda57e637c02a88f165876ab9f0aa180d1
bkkavin <bkkavin2000@gmail.com>
2021-12-31 21:23:56 +0530

    foo

--- xmonad.hs
+++ xmonad.hs
@@ -46,24 +46,22 @@ defaults = def
         }
 
 
-myLayoutHook = noBorders(Full) |||  ThreeCol 1 (3/100) (1/3) ||| ThreeColMid 1 (3/100) (1/2) ||| magnifier (Tall 1 (3/100) (1/2)) 
+myLayoutHook =  noBorders(Full) ||| ThreeColMid 1 (3/100) (1/2) ||| magnifier (Tall 1 (3/100) (1/2)) ||| ThreeCol 1 (3/100) (1/3) 
 myTerminal = "urxvt"
 myModMask  = mod4Mask
 myManageHook = manageSpawn <+> manageHook def 
 
 
 myStartupHook = do
-                spawnOn "1" "emacs"
-                spawnOn "2" "brave"
-                spawnOn "3" "urxvt"
-                spawnAndDo (doRectFloat (W.RationalRect 0.25 0.25 0.5 0.5) <+> doShift 1) "emacs"
+                spawnOn "2" "emacs"
+                spawnOn "3" "brave"
+                spawnOn "4" "urxvt"
+                spawnAndDo (doRectFloat (W.RationalRect 0.25 0.25 0.5 0.5) <+> doShift "1") "emacs"
 
-{-spawnOn "4" "urxvt ~/echo.sh" -}
 
 
 
    
 
-{- manageDashboard = composeAll  -}
 
 
