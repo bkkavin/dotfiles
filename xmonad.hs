e0d7d423bbb04ec36850c49375c560b82bf105d2
bkkavin <bkkavin2000@gmail.com>
2022-01-04 20:10:42 +0530

    dotfiles update

--- xmonad.hs
+++ xmonad.hs
@@ -22,9 +22,28 @@ import XMonad.Layout.Grid
 import XMonad.Layout.Renamed
 import XMonad.Util.SpawnOnce
 import XMonad.Util.Run
+import XMonad.Hooks.ManageDocks
+
+
+
+myLayoutHook = ThreeCol 1 (3/100) (2/3)  |||  noBorders(Full)  ||| centerMaster Grid   
+myTerminal = "urxvt"
+myModMask  = mod4Mask
+myManageHook = manageSpawn <+> manageHook def 
+
+
+myStartupHook = do
+                spawnOn "2" "emacs"
+                spawnAndDo (doShift "3") "brave" 
+                {- spawnOn "3" "brave" -}
+                spawnOn "4" "urxvt"
+                spawnAndDo (doRectFloat (W.RationalRect 0.05 0.1 0.4 0.8) <+> doShift "1") "emacs" 
+
 
 main :: IO ()
-main =  xmonad def        
+main = do
+xmproc <- spawnPipe "xmobar -x 0 ~/.xmobarrc"
+xmonad def
        {terminal           = myTerminal,
         {- focusFollowsMouse  = myFocusFollowsMouse,
         clickJustFocuses   = myClickJustFocuses,
@@ -45,24 +64,4 @@ main =  xmonad def
        logHook            = myLogHook -}
         startupHook    = myStartupHook
         }
-
-
-myLayoutHook = ThreeCol 1 (3/100) (1/2)  |||  noBorders(Full)  ||| centerMaster Grid   
-myTerminal = "urxvt"
-myModMask  = mod4Mask
-myManageHook = manageSpawn <+> manageHook def 
-
-
-myStartupHook = do
-                spawnOn "2" "emacs"
-                spawnOn "3" "brave"
-                spawnOn "4" "urxvt"
-                spawnAndDo (doRectFloat (W.RationalRect 0.05 0.1 0.4 0.8) <+> doShift "1") "emacs" 
-
-
-
-
-   
-
-
 
