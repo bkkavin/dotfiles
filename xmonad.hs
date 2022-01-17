6052eebc8c01b5d2d4808f69eff1676d4e85b397
bkkavin <bkkavin2000@gmail.com>
2022-01-18 00:27:01 +0530

    dotfiles update

--- xmonad.hs
+++ xmonad.hs
@@ -20,6 +20,9 @@ import XMonad.Layout.CenteredMaster
 import XMonad.Layout.Grid
 import XMonad.Layout.Renamed
 import XMonad.Actions.Submap
+import XMonad.Layout.Gaps
+import XMonad.Layout.Spacing
+import XMonad.Hooks.ManageDocks
 
 
 import XMonad.Util.SpawnOnce
@@ -28,15 +31,21 @@ import XMonad.Util.EZConfig
 import XMonad.Util.NamedScratchpad
 --import XMonad.Hooks.EwmhDesktops
 
+myLayoutHook = gaps [(U,20) , (D,20) , (L,20) , (R,20)] $ ThreeCol 1 (3/100) (1/2)  |||  Full  ||| centerMaster Grid   
 
-
-myLayoutHook = ThreeCol 1 (3/100) (1/2)  |||  noBorders(Full)  ||| centerMaster Grid   
 myTerminal = "urxvt"
 myModMask  = mod4Mask
 myNormalBorderColor =  "#000000" --"#F7F7F7"
 myFocusedBorderColor =  "#334257" --"#EEEEEE"
 myBorderWidth = 3 
-myManageHook = manageSpawn <+> manageHook def<+> namedScratchpadManageHook myScratchPads
+myManageHook = manageSpawn <+> manageHook def <+> namedScratchpadManageHook myScratchPads <+> composeAll . concat  $
+        [ [(className =? x <||> title =? x <||> resource =? x) --> doShift "2" | x <- my2Shifts]
+        ]
+        where
+        my2Shifts = ["Brave"]
+
+
+
 
 myScratchPads :: [NamedScratchpad]
 myScratchPads = [ NS "terminal" spawnTerm findTerm manageTerm
@@ -60,21 +69,25 @@ myScratchPads = [ NS "terminal" spawnTerm findTerm manageTerm
                  w = 0.98
                  y = (1.025 - h) / 2
                  x = (1 - w) / 2
-		 
+
 
 
 myStartupHook = do
-                spawnAndDo (doRectFloat (W.RationalRect 0.1 0.1 0.4 0.8) <+> doShift "1") "emacs" 
-                spawnOn "2" "brave"
-                spawnAndDo (doRectFloat (W.RationalRect 0.55 0.1 0.4 0.8) <+> doShift "1") "urxvt -hold" 
+        --      spawnAndDo (doRectFloat (W.RationalRect 0.1 0.1 0.4 0.8)) "emacs"
+                spawnAndDo (doShift "1") "emacs"
+                spawnAndDo (doShift "1") "urxvt"
+
+                spawnAndDo (doShift "2") "brave"
+        --      spawnAndDo (doRectFloat (W.RationalRect 0.55 0.1 0.4 0.8)) "urxvt"
                 spawnOnce "emacs --daemon=xmonad"
- a              spawnOnce " urxvtd -q -o -f "
+                spawnOnce " urxvtd -q -o -f "
 myKeys :: [(String, X ())]
 myKeys = 
         [ ("M-C-r", spawn "xmonad --recompile")       -- Recompiles xmonad
-        , ("C-M1-t", namedScratchpadAction myScratchPads "terminal")
-        , ("C-M1-s", namedScratchpadAction myScratchPads "ecx")
-
+        , ("C-M4-t", namedScratchpadAction myScratchPads "terminal")
+        , ("C-M4-s", namedScratchpadAction myScratchPads "ecx")
+        , ("C-M4-m", sendMessage $ ToggleGaps)     
+        , ("C-M4-h", sendMessage ToggleStruts)
         ]
 
 
@@ -82,7 +95,7 @@ myKeys =
 main :: IO ()
 main = do
 xmproc <- spawnPipe "xmobar -x 0 ~/.xmobarrc"
-xmonad  $ def
+xmonad $ docks $ def
       {terminal            = myTerminal
   {-  ,  focusFollowsMouse  = myFocusFollowsMouse
       ,  clickJustFocuses   = myClickJustFocuses      -}
@@ -97,7 +110,7 @@ xmonad  $ def
   --  , mouseBindings      = myMouseBindings           
 
       -- hooks, layouts
-      , layoutHook         = myLayoutHook
+      , layoutHook         = smartSpacingWithEdge 20 $ myLayoutHook
       , manageHook         = myManageHook
   {-  , handleEventHook    = myEventHook 
       , logHook            = myLogHook                  -}
