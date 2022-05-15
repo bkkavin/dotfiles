6188906cb03e3766f4fc41bfec190c16a7115af1
bkk-ubuntu <bkkavin2000@gmail.com>
2022-05-15 22:12:27 +0530

    -

--- guix/config.scm
+++ guix/config.scm
@@ -1,20 +1,20 @@
 ;; Import nonfree linux module.
-(use-modules (gnu packages)
-	     (nongnu packages linux)
-             (nongnu system linux-initrd))
+;; (use-modules (gnu packages)
+;; 	     (nongnu packages linux)
+;;              (nongnu system linux-initrd))
 
 ;; This is an operating system configuration template
 ;; for a "bare bones" setup, with no X11 display server.
 
-(use-modules (gnu))
-(use-service-modules networking ssh)
-(use-package-modules screen ssh)
+ (use-modules (gnu))
+;; (use-service-modules networking ssh)
+;; (use-package-modules screen ssh)
 
 (operating-system
 
- (kernel linux)
- (initrd microcode-initrd)
- (firmware (list linux-firmware))
+ ;; (kernel linux)
+ ;; (initrd microcode-initrd)
+ ;; (firmware (list linux-firmware))
 
  (host-name "bkk")
  (timezone "Asia/Kolkata")
@@ -24,20 +24,20 @@
  ;; target hard disk, and "my-root" is the label of the target
  ;; root file system.
  (bootloader (bootloader-configuration
-	      (bootloader grub-bootloader)
-	      (targets '("/dev/sda5"))))
+ 	      (bootloader grub-bootloader)
+ 	      (targets '("/dev/sda5"))))
  (file-systems (cons (file-system
-		      (device (file-system-label "guix-root"))
-		      (mount-point "/")
-		      (type "ext4"))
-		     %base-file-systems))
+ 		      (device (file-system-label "guix-root"))
+ 		      (mount-point "/")
+ 		      (type "ext4"))
+ 		     %base-file-systems))
 
  ;; This is where user accounts are specified.  The "root"
  ;; account is implicit, and is initially created with the
  ;; empty password.
  (users (cons (user-account
-	       (name "alice")
-	       (comment "Bob's sister")
+	       (name "bkk")
+	       (comment "B K Kavin")
 	       (group "users")
 
 	       ;; Adding the account to the "wheel" group
