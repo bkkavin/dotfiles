5ea1d7fb54f3e36d4cc98f79bb1c8acfbc33ae82
bkk-ubuntu <bkkavin2000@gmail.com>
2022-09-18 14:01:53 +0530

    major dotfiles update; stow dir created

--- guix/bkk-iso.scm
+++ guix/bkk-iso.scm
@@ -1,36 +1,28 @@
-;; This is an operating system configuration template
-;; for a "desktop" setup with GNOME and Xfce where the
-;; root partition is encrypted with LUKS.
-
-(use-modules (gnu) (gnu system nss))
+(use-modules
+ (gnu) (gnu system nss)
+ (nongnu packages linux)
+ (nongnu system linux-initrd)
+)
 (use-service-modules desktop xorg)
-(use-package-modules certs gnome)
+(use-package-modules certs gnome lisp wm)
 
 (operating-system
   (host-name "bkk")
   (timezone "Asia/Kolkata")
   (locale "en_US.utf8")
 
-  ;; Choose US English keyboard layout.  The "altgr-intl"
-  ;; variant provides dead keys for accented characters.
+  (kernel linux)
+  (initrd microcode-initrd)
+  (firmware (list linux-firmware))
+
   (keyboard-layout (keyboard-layout "us" "altgr-intl"))
 
-  ;; Use the UEFI variant of GRUB with the EFI System
-  ;; Partition mounted on /boot/efi.
   (bootloader (bootloader-configuration
                 (bootloader grub-efi-bootloader)
                 (targets "/boot/efi")
                 (keyboard-layout keyboard-layout)))
 
-  ;; Specify a mapped device for the encrypted root partition.
-  ;; The UUID is that returned by 'cryptsetup luksUUID'.
-  (mapped-devices
-   (list (mapped-device
-          (source (uuid "580461b7-c685-4d25-95da-bc630b09dec1"))
-          (targets "guix-fs")
-	  (type lvm-device-mapping))))
-
-  (file-systems (append
+   (file-systems (append
                  (list (file-system
                          (device (uuid "580461b7-c685-4d25-95da-bc630b09dec1"))
                          (mount-point "/")
@@ -41,32 +33,27 @@
                          (type "vfat")))
                  %base-file-systems))
 
-  ;; Create user `bob' with `alice' as its initial password.
   (users (cons (user-account
                 (name "bkk")
-                (password (crypt "bkk" "697803"))
                 (group "users")
                 (supplementary-groups '("wheel" "netdev"
                                         "audio" "video")))
                %base-user-accounts))
 
-  ;; This is where we specify system-wide packages.
   (packages (append (list
+		     ;; stumpwm
+		     sbcl stumpwm `(,stumpwm "lib")
                      ;; for HTTPS access
                      nss-certs
                      ;; for user mounts
                      gvfs)
-                    %base-packages))
+                     ;; sbcl-ttf-fonts font-dejavu
+		    %base-packages))
 
-  ;; Add GNOME and Xfce---we can choose at the log-in screen
-  ;; by clicking the gear.  Use the "desktop" services, which
-  ;; include the X11 log-in service, networking with
-  ;; NetworkManager, and more.
-  (services (append (list (service xfce-desktop-service-type)
+ (services (append (list (service xfce-desktop-service-type)
                           (set-xorg-configuration
                            (xorg-configuration
                             (keyboard-layout keyboard-layout))))
                     %desktop-services))
 
-  ;; Allow resolution of '.local' host names with mDNS.
   (name-service-switch %mdns-host-lookup-nss))
--- stumpwmrc.lisp
+++ stumpwmrc.lisp
@@ -208,3 +208,10 @@
 
 ;; (defcommand brave-bkk ()() (run-or-raise "brave-browser" '(:class "Brave-browser")))
 ;; (define-key *root-map* (kbd "b") "brave-bkk") 
+
+;; (require :ttf-fonts)
+;;      (setf xft:*font-dirs* '("/run/current-system/profile/share/fonts/"))
+;;      (setf clx-truetype:+font-cache-filename+ (concat (getenv "HOME") "/.fonts/font-cache.sexp"))
+;;      (xft:cache-fonts)
+;;      (set-font (make-instance 'xft:font :family "DejaVu Sans Mono" :subfamily "Book" :size 11))
+
